55cba85ba9d772313ebfc8e13ce6b82db9e2beb0
Yaseen Khan <kyaseen49@yahoo.com>
2022-10-06 13:24:54 -0700

    Added necessary libarires to servlet files

--- src/main/MoviesServlet.java
+++ src/main/MoviesServlet.java
@@ -1,7 +1,19 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 @WebServlet(najme = "MoviesServlet", urlPatterns = "/movies")
 public class MoviesServlet extends HttpServlet{
