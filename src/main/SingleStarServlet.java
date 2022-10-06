5731075c1cbd7de3a0a9bf43c43696e2de818543
Yaseen Khan <kyaseen49@yahoo.com>
2022-10-06 16:59:14 -0700

    Began implementation of Single Star

--- src/main/SingleStarServlet.java
+++ src/main/SingleStarServlet.java
@@ -14,13 +14,57 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
 
 @WebServlet(name = "SingleStarServlet", urlPatterns = "/single-star")
 public class SingleStarServlet extends HttpServlet{
+
+    // Creates a datasource from the web.xml file
+    private DataSource dataSource;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         /**
          * TODO: Implement JDBC to retrieve star information and return in a json format.
          */
+
+        // Set the response to be a JSON object
+        response.setContentType("application/json"); // Response mime type
+
+        // retrieves parameter id from url request
+        String id = request.getParameter("id");
+
+        // log message for debugging
+        request.getServletContext().log("getting id: " + id);
+
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
+
+        // Establish connection with database and closes connection after being used
+        try (Connection conn = dataSource.getConnection()) {
+
+            // Construct a query with parameter based on ?
+            // ? = parameter
+            final String query = "SELECT";
+
+            // Declare our statement
+        } catch (Exception e) {
+            // Write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            // Log the error for debugging
+            request.getServletContext().log("Error:", e);
+
+            // Set Error code status to 500(Internal Server Error)
+            response.setStatus(500);
+        } finally {
+            // close the connection
+            out.close();
+        }
+
+
+        // Perform the query
+        // ResultSet rs = s
     }
 }
