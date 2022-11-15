5943656f13e670138f33ec6c9950862461ea9548
jadonk42 <jadonkwan2@gmail.com>
2022-11-14 20:38:49 -0800

    Implemented all autocomplete features

--- WebContent/search.js
+++ WebContent/search.js
@@ -7,26 +7,63 @@
  */
 function handleLookup(query, doneCallback) {
     console.log("autocomplete initiated")
-    console.log("sending AJAX request to backend Java Servlet")
 
-    // TODO: if you want to check past query results first, you can do it here
+    let cachedData = sessionStorage.getItem(query);
+    if (cachedData != null) {
+        console.log("query found in cache.")
+        cachedData = JSON.parse(cachedData);
+        console.log(cachedData);
+        doneCallback({ suggestions: cachedData });
+        return;
+    }
 
     // sending the HTTP GET request to the Java Servlet endpoint hero-suggestion
     // with the query data
+    console.log("sending AJAX request to backend Java Servlet")
     jQuery.ajax({
         "method": "GET",
         // generate the request url from the query.
         // escape the query string to avoid errors caused by special characters
-        "url": "hero-suggestion?query=" + escape(query),
-        "success": function(data) {
+        "url": "api/autocomplete?full_text=" + escape(query),
+        "success": function(resultData) {
             // pass the data, query, and doneCallback function into the success handler
-            handleLookupAjaxSuccess(data, query, doneCallback)
+            handleLookupAjaxSuccess(resultData, query, doneCallback)
         },
         "error": function(errorData) {
             console.log("lookup ajax error")
             console.log(errorData)
         }
     })
+}
+
+
+/*
+ * This function is used to handle the ajax success callback function.
+ * It is called by our own code upon the success of the AJAX request
+ *
+ * data is the JSON data string you get from your Java Servlet
+ *
+ */
+function handleLookupAjaxSuccess(resultData, query, doneCallback) {
+    // console.log("lookup ajax successful")
+    console.log(resultData);
+
+    sessionStorage.setItem(query, JSON.stringify(resultData));
+    // call the callback function provided by the autocomplete library
+    // add "{suggestions: jsonData}" to satisfy the library response format according to
+    //   the "Response Format" section in documentation
+    doneCallback( { suggestions: resultData } );
+}
+
+
+/*
+ * This function is the select suggestion handler function.
+ * When a suggestion is selected, this function is called by the library.
+ *
+ * You can redirect to the page you want using the suggestion data.
+ */
+function handleSelectSuggestion(suggestion) {
+    window.location.replace("single-movie.html?id=" + suggestion["data"]);
 }
 
 
@@ -50,6 +87,7 @@ jQuery('#autocomplete').autocomplete({
     },
     // set delay time
     deferRequestBy: 300,
+    minChars: 3
     // there are some other parameters that you might want to use to satisfy all the requirements
     // TODO: add other parameters, such as minimum characters
 });
---
+++ src/main/java/AutocompleteSearchServlet.java
@@ -0,0 +1,113 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.sql.DataSource;
+import java.net.HttpURLConnection;
+import java.sql.*;
+
+@WebServlet(name = "AutocompleteSearchServlet", urlPatterns = "/api/autocomplete")
+public class AutocompleteSearchServlet extends HttpServlet{
+    private static final long serialVersionUID = 1L;
+
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+
+        String full_text = request.getParameter("full_text");
+        full_text = transformFullText(full_text);
+
+        try (Connection conn = dataSource.getConnection()) {
+            PreparedStatement statement;
+            statement = conn.prepareStatement(getQueryStatement(full_text));
+
+            System.out.println(statement);
+            ResultSet rs = statement.executeQuery();
+            JsonArray jsonArray = new JsonArray();
+
+            while (rs.next()) {
+                jsonArray.add(getMoviesAsJson(rs));
+            }
+
+            System.out.println("done getting json");
+
+            //JsonObject resultData = new JsonObject();
+            //resultData.addProperty("suggestions", jsonArray);
+
+            rs.close();
+            statement.close();
+            request.getServletContext().log("getting " + jsonArray.size() + " results");
+            out.write(jsonArray.toString());
+
+            response.setStatus(HttpURLConnection.HTTP_OK);
+
+        } catch (Exception e) {
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            request.getServletContext().log("Error:", e);
+            response.setStatus(HttpURLConnection.HTTP_INTERNAL_ERROR);
+        } finally {
+            out.close();
+        }
+    }
+
+
+    private String getQueryStatement(String full_text) {
+        String topMoviesResults = "SELECT DISTINCT(m.id), m.title, r.rating " +
+                "FROM ratings as r RIGHT JOIN (movies as m, stars_in_movies as sm, stars as s)  ON (m.id = r.movieId)" +
+                "WHERE m.id = sm.movieId AND s.id = sm.starId AND " +
+                "MATCH m.title AGAINST ('" + full_text + "' IN BOOLEAN MODE) " +
+                "ORDER BY r.rating LIMIT 10;";
+
+        return topMoviesResults;
+    }
+
+
+
+    private String transformFullText(String full_text) {
+        String[] arrayString = full_text.split("[, ]");
+        String result = "";
+
+        for (String token: arrayString) {
+            if (token != "") {
+                result += "+" + token + "*";
+            }
+        }
+        return result;
+    }
+
+    private JsonObject getMoviesAsJson(ResultSet rs) throws SQLException {
+        String movieId = rs.getString("m.id");
+        String movieTitle = rs.getString("m.title");
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("data", movieId);
+        jsonObject.addProperty("value", movieTitle);
+
+        return jsonObject;
+    }
+}
+
+
